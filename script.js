47517b5ff9e6b40e81d2dee8eafc8efe358a8028
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2022-08-22 13:51:49 +0200

    Ejercicio 18

---
+++ script.js
@@ -0,0 +1,9 @@
+let revelarCarta = () =>{
+    let x = document.getElementById("imagen");
+    x.src = "./imgs/card-front.png";
+}
+
+let esconderCarta = () =>{
+    let x = document.getElementById("imagen");
+    x.src = "./imgs/card-back.png";
+}
